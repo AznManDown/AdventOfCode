c1c6f65a225ca3a67cb5081fb3f23b8a19334f8b
AznManDown <aznmandown@gmail.com>
2020-05-15 16:14:15 -0700

    Added plotRoute

--- Day3/Wires.java
+++ Day3/Wires.java
@@ -3,6 +3,38 @@ import java.io.*;
 
 public class Wires {
 
+    public static void plotRoute(ArrayList<Character> direction, ArrayList<Integer> location) {
+
+        String[][] plotGrid = new String[20000][20000];
+        int xCord = 10000;
+        int yCord = 10000;
+
+        for (int i = 0; i < direction.size(); i++) {
+            switch (direction.get(i)) {
+                case 'R':
+                    plotGrid[xCord][yCord + location.get(i)] = "X";
+                    yCord = yCord + location.get(i);
+                    System.out.println("X: " + xCord + ", Y: " + yCord);
+                    break;
+                case 'L':
+                    plotGrid[xCord][yCord - location.get(i)] = "X";
+                    yCord = yCord - location.get(i);
+                    System.out.println("X: " + xCord + ", Y: " + yCord);
+                    break;
+                case 'U':
+                    plotGrid[xCord + location.get(i)][yCord] = "X";
+                    xCord = xCord + location.get(i);
+                    System.out.println("X: " + xCord + ", Y: " + yCord);
+                    break;
+                case 'D':
+                    plotGrid[xCord + location.get(i)][yCord] = "X";
+                    xCord = xCord - location.get(i);
+                    System.out.println("X: " + xCord + ", Y: " + yCord);
+                    break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         //Read input of two wires. Sort into two arrays.
         String firstMem = "";
@@ -24,25 +56,25 @@ public class Wires {
         String[] firstWire = firstMem.split(",");
         String[] secondWire = secondMem.split(",");
         ArrayList<Character> firstDirection = new ArrayList<Character>();
-        ArrayList<String> firstLoc = new ArrayList<String>();
+        ArrayList<Integer> firstLoc = new ArrayList<Integer>();
         ArrayList<Character> secondDirection = new ArrayList<Character>();
-        ArrayList<String> secondLoc = new ArrayList<String>();
+        ArrayList<Integer> secondLoc = new ArrayList<Integer>();
 
         for (String direction :  firstWire) {
             Character dInput = direction.charAt(0);
             String lInput = direction.replaceAll("[A-Z]", "");
             firstDirection.add(dInput);
-            firstLoc.add(lInput);
+            firstLoc.add(Integer.parseInt(lInput));
         }
 
         for (String direction :  secondWire) {
             Character dInput = direction.charAt(0);
             String lInput = direction.replaceAll("[A-Z]", "");
             secondDirection.add(dInput);
-            secondLoc.add(lInput);
+            secondLoc.add(Integer.parseInt(lInput));
         }
 
-
+        plotRoute(firstDirection, firstLoc);
 
     }
 }
