3e9b09ec0301fd63aaa8b5346bb8e7465d022399
AznManDown <aznmandown@gmail.com>
2020-06-03 10:21:12 -0700

    Interval Update

--- Day6/Orbit.java
+++ Day6/Orbit.java
@@ -1,7 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Orbit {
+
+    public static LinkedList<String> readInput() {
+        LinkedList<String> mList = new LinkedList<>();
+        try (BufferedReader bReader = new BufferedReader(new FileReader("Day6/TestInput"))) {
+            String sCurrentLine;
+
+            while((sCurrentLine = bReader.readLine()) != null) {
+                mList.add(sCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mList;
+    }
+
+    public static List<Tree> createTree(LinkedList<String> data) {
+
+        String rootNode = "COM";
+        Boolean setRoot = false;
+
+        for (String node : data) {
+            if (setRoot == false) {
+                if (node.startsWith("COM") == true) {
+
+                }
+            }
+        }
+
+        Tree root = new Tree("A", null);
+        Tree nodeB = new Tree("B", root);
+        root.children.add(nodeB);
+        Tree nodeC = new Tree("C", nodeB);
+        nodeB.children.add(nodeC);
+        Tree nodeD = new Tree("D", nodeC);
+        nodeC.children.add(nodeD);
+
+        return root.children;
+    }
 
     public static void main(String[] args) {
 
+        LinkedList<String> orbitData = new LinkedList<String>(readInput());
+        List<Tree> orbitTree = new LinkedList<>(createTree(orbitData));
 
+        System.out.println(orbitTree.get(0).data);
     }
 }
--- Day6/Tree.java
+++ Day6/Tree.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 public class Tree {
     public List<Tree> children = new LinkedList<Tree>();
-    private Tree parent = null;
-    private String data;
+    public Tree parent = null;
+    public String data;
 
     public Tree(String data, Tree parent) {
         this.data = data;
