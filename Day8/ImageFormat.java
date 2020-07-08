58613bebe0d07375ff17492b47c04461cab05fcd
AznManDown <aznmandown@gmail.com>
2020-07-08 14:52:11 -0700

    Day 8 - Complete

--- Day8/ImageFormat.java
+++ Day8/ImageFormat.java
@@ -36,9 +36,74 @@ public class ImageFormat {
         return twoDArray;
     }
 
+    public static int findImage(int[][] data) {
+        int zeroCount = 0;
+
+        for (int row = 0; row < data.length; row++) {
+            for (int col = 0; col < data[row].length; col++) {
+                if (data[row][col] == 0) {
+                    zeroCount++;
+                }
+            }
+        }
+        return zeroCount;
+    }
+
+    public static void digitMulti(int[][] data) {
+        int oneCount = 0;
+        int twoCount = 0;
+        int multiResult;
+
+        for (int row = 0; row < data.length; row++) {
+            for (int col = 0; col < data[row].length; col++) {
+                if (data[row][col] == 1) {
+                    oneCount++;
+                }
+
+                if (data[row][col] == 2) {
+                    twoCount++;
+                }
+            }
+        }
+
+        multiResult = oneCount * twoCount;
+
+        System.out.println(multiResult);
+    }
+
+    public static void generateImage(HashMap<Integer, int[][]> data) {
+        int[][] generatedImage = new int[6][25];
+
+        //generatedImage init.
+        for (int row = 0; row < generatedImage.length; row++) {
+            for (int col = 0; col < generatedImage[row].length; col++) {
+                generatedImage[row][col] = 2;
+            }
+        }
+
+        for (int cLayer = 0; cLayer < data.size(); cLayer++) {
+            int[][] lData = data.get(cLayer);
+
+            for (int row = 0; row < lData.length; row++) {
+                for (int col = 0; col < lData[row].length; col++) {
+                    if (generatedImage[row][col] == 2) {
+                        generatedImage[row][col] = lData[row][col];
+                    }
+                }
+            }
+        }
+
+        for (int row = 0; row < generatedImage.length; row++) {
+            for (int col = 0; col < generatedImage[row].length; col++) {
+                System.out.print(generatedImage[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         //Read Instruction List
-        ArrayList<BigInteger> instructionList = readProgram("Day8/TestInput");
+        ArrayList<BigInteger> instructionList = readProgram("Day8/input");
         BigInteger number = instructionList.get(0);
         LinkedList<Integer> stack = new LinkedList<>();
 
@@ -51,20 +116,42 @@ public class ImageFormat {
         //Final Instruction Push
         stack.push(number.intValue());
 
-        LinkedList<ArrayList<Integer>> tempList = new LinkedList<>();
+        LinkedList<ArrayList<Integer>> chunkTemp = new LinkedList<>();
+        LinkedList<Integer> resultCount = new LinkedList<>();
         HashMap<Integer, int[][]> dataStorage = new HashMap<>();
+        int zeroCount = 0;
 
-        for (int i=0; i < stack.size(); i += 6) {
+        //Split instructions into proper sized chunks
+        for (int i=0; i < stack.size(); i += 150) {
             ArrayList<Integer> tempArray = new ArrayList<>();
-            for (int x=i; x < i+6; x++) {
+            for (int x=i; x < i+150; x++) {
                 tempArray.add(stack.get(x));
             }
-            tempList.add(tempArray);
+            chunkTemp.add(tempArray);
         }
 
-        for (int i=0; i < tempList.size(); i++) {
-            dataStorage.put(i, buildImage(tempList.get(i), 3, 2));
+        //Add chunks to HashMap for storage
+        for (int i=0; i < chunkTemp.size(); i++) {
+            dataStorage.put(i, buildImage(chunkTemp.get(i), 25, 6));
         }
+
+        for (int x = 0; x < dataStorage.size(); x++) {
+            resultCount.add(findImage(dataStorage.get(x)));
+        }
+
+        for (int y = 0; y < resultCount.size(); y++) {
+            int cCount = resultCount.get(y);
+            if (y == 0) {
+                zeroCount = cCount;
+            }
+
+            if (cCount < zeroCount) {
+                zeroCount = cCount;
+            }
+        }
+
+        //digitMulti(dataStorage.get(resultCount.indexOf(zeroCount)));
+        generateImage(dataStorage);
 
 
 
